793f136bb9c25ef3dc891d157ee91cb9f6d47e3a
Tara Z. Manicsic <tzmanics@gmail.com>
2019-12-10 12:16:51 -0500

    adds routing and routerlinks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+import { ReferencesComponent } from "./references/references.component";
+import { TutorialsComponent } from "./tutorials/tutorials.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "about", component: AboutComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "references", component: ReferencesComponent },
+  { path: "tutorials", component: TutorialsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
